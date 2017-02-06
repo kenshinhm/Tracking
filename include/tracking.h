c2dbbca5c03e59e5645726412ea7249cf6b6e719
jaehyoungyoo <kenshinhm@naver.com>
2017-02-06 20:22:01 +0900

    fix: change in tracking.h, cv::Point2d -> cv::point2f

--- include/tracking.h
+++ include/tracking.h
@@ -1,9 +1,9 @@
 #include "math.h"
 #include "string"
-#include "sstream"
 #include "iostream"
 #include "vector"
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
+#include "opencv2/video/tracking.hpp"
 
 using namespace std;
 
@@ -28,16 +28,16 @@ public:
 private:
     cv::Rect GetROI(int x, int y, int width, int height);
     cv::Rect GetROI(cv::Rect roi);
-    void SetPointsGlobal(vector<cv::Point2d>& points, cv::Rect roi);
-    void SetPointsLocal(vector<cv::Point2d>& points, cv::Rect roi);
+    void SetPointsGlobal(vector<cv::Point2f>& points, cv::Rect roi);
+    void SetPointsLocal(vector<cv::Point2f>& points, cv::Rect roi);
     void Resize(cv::Rect& rect, double scale);
-    void Resize(vector<cv::Point2d>& points, double scale);
+    void Resize(vector<cv::Point2f>& points, double scale);
     void RectToPoints(cv::Rect rect, vector<cv::Point>& points);
-    void RectToPoints(cv::Rect rect, vector<cv::Point2d>& points);
-    void GetCorners(cv::Mat image, vector<cv::Point2d>& ret ,CORNER corner, int step);
-    cv::Rect Rearrange(vector<cv::Point2d>& prev_matches, vector<cv::Point2d>& next_matches, cv::Rect prev_roi, int step, double threshold);
-
-    cv::Rect Transform(vector<cv::Point2d>& prev_matches, vector<cv::Point2d>& next_matches, cv::Rect prev_roi, METHOD method);
-    void OpticalFlowPyLK(cv::Mat prev, cv::Mat next, vector<cv::Point2d> prev_corners,
-                         vector<cv::Point2d>& prev_matches, vector<cv::Point2d>& next_matches);
+    void RectToPoints(cv::Rect rect, vector<cv::Point2f>& points);
+    void GetCorners(cv::Mat image, vector<cv::Point2f>& ret ,CORNER corner, int step);
+    cv::Rect Rearrange(vector<cv::Point2f>& prev_matches, vector<cv::Point2f>& next_matches, cv::Rect prev_roi, int step, double threshold);
+    cv::Rect Transform(vector<cv::Point2f>& prev_matches, vector<cv::Point2f>& next_matches, cv::Rect prev_roi, METHOD method);
+    void OpticalFlowPyLK(cv::Mat prev, cv::Mat next, vector<cv::Point2f> prev_corners,
+                         vector<cv::Point2f> prev_estimate, vector<cv::Point2f> next_estimate,
+                         vector<cv::Point2f>& prev_matches, vector<cv::Point2f>& next_matches);
 };
